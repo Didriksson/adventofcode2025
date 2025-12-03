ba62ba9d8cc4a2a963c96853bd75f3a17a3b351e
Mattias Didriksson <matdid@lm.se>
2025-12-03 10:49:04 +0100

    Bort med main

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -3,26 +3,29 @@ import kotlin.text.mapIndexed
 class Day03 {
 
     fun part1(input: List<String>): Long {
-        val batteries = input.map { findSuperBattery( 2, it) }
+        val batteries = input.map { findSuperBattery(2, it) }
         return batteries.sum()
     }
 
     fun part2(input: List<String>): Long {
-        val batteries = input.map { findSuperBattery( 12, it) }
+        val batteries = input.map { findSuperBattery(12, it) }
         return batteries.sum()
     }
 
     private fun findHighest(batteryarray: String): Pair<Int, Long> {
-        val maxBy = batteryarray.mapIndexed { index, battery -> Pair(index, battery.digitToInt().toLong()) }.maxBy { it.second }
-        return maxBy
+        return batteryarray.mapIndexed { index, battery -> Pair(index, battery.digitToInt().toLong()) }
+            .maxBy { it.second }
     }
 
     private fun findSuperBattery(size: Int, batteryarray: String): Long {
-        return if(size == 1){
+        return if (size == 1) {
             findHighest(batteryarray).second
-        } else{
-            val highest = findHighest(batteryarray.dropLast(size-1))
-            return (highest.second.toString() + findSuperBattery(size-1, batteryarray.drop(highest.first+1))).toLong()
+        } else {
+            val highest = findHighest(batteryarray.dropLast(size - 1))
+            return (highest.second.toString() + findSuperBattery(
+                size - 1,
+                batteryarray.drop(highest.first + 1)
+            )).toLong()
         }
 
     }
